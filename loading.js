881746061af220500a11a763a6df3199490896d7
niraj.maharjan <niraj.maharjan@dragonlaw.com.hk>
2018-03-28 17:29:39 +0545

    add git ignore file

---
+++ loading.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class Loading extends React.Component {
+	static propTypes = {
+		loading: PropTypes.bool,
+		children: PropTypes.object
+	};
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: false
+		}
+	}
+
+	componentDidMount() {
+		const loading = this.state.loading;
+		this.setState({
+			loading
+		})
+	}
+
+	componentWillReceiveProps(nextProps) {
+		const {loading} = nextProps
+		this.setState({
+			loading
+		})
+	}
+
+	render() {
+		const loading = this.state.loading;
+		const Spinner = this.props.spinner;
+
+		return (
+			loading ? (Spinner ? <div><Spinner />Loading spinner</div>: 'Loading...') : this.props.children
+		);
+	}
+}
+
+A Simple high order component (HOC) that helps to show loading sign untill child component is ready
